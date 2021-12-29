6ba416178a464d0ad63334f259d5548fdb68273e
Antonio <ar0x2ff@gmail.com>
2021-12-29 10:41:59 +0100

    service annotation

---
+++ src/main/java/service/Service.java
@@ -0,0 +1,16 @@
+package service;
+
+import javax.interceptor.InterceptorBinding;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target({TYPE, METHOD})
+@Retention(RUNTIME)
+@InterceptorBinding
+public @interface Service
+{}
