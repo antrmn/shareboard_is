d27465adee05cdf2e21e116bd7c4eca51555414c
Zane <alessandro.demaio@virgilio.it>
2022-02-07 22:50:45 +0100

    -FollowServletTest -UnfollowServletTest

---
+++ src/test/java/http/controller/FollowServletTest.java
@@ -0,0 +1,61 @@
+package http.controller;
+
+
+import org.apache.openejb.testing.Classes;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import rocks.limburg.cdimock.CdiMock;
+import service.FollowService;
+
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.ConstraintViolationException;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+
+@Classes(cdi = true,
+        value={FollowServlet.class},
+        cdiStereotypes = CdiMock.class)
+public class FollowServletTest extends ServletTest{
+
+    @Mock private FollowService service;
+    @Mock private HttpServletRequest request;
+    @Mock private HttpServletResponse response;
+    @Inject FollowServlet followServlet;
+
+    @Test
+    void successfulldoPost() throws ServletException, IOException{
+        when(request.getParameter("section")).thenReturn("1");
+        followServlet.doPost(request,response);
+        verify(service, times(1)).follow(anyInt());
+    }
+
+    @Test
+    void faildoPost() throws ServletException, IOException{
+        when(request.getParameter("section")).thenReturn("1");
+        when(service.follow(anyInt())).thenThrow(ConstraintViolationException.class);
+        assertThrows(ConstraintViolationException.class,() -> followServlet.doPost(request,response));
+    }
+
+
+    @Test
+    void successfulldoGet() throws ServletException, IOException{
+        when(request.getParameter("section")).thenReturn("1");
+        followServlet.doGet(request,response);
+        verify(service, times(1)).follow(anyInt());
+    }
+
+    @Test
+    void faildoGet() throws ServletException, IOException{
+        when(request.getParameter("section")).thenReturn("1");
+        when(service.follow(anyInt())).thenThrow(ConstraintViolationException.class);
+        assertThrows(ConstraintViolationException.class,() -> followServlet.doGet(request,response));
+    }
+
+
+}
---
+++ src/test/java/http/controller/UnfollowServletTest.java
@@ -0,0 +1,61 @@
+package http.controller;
+
+
+import org.apache.openejb.testing.Classes;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import rocks.limburg.cdimock.CdiMock;
+import service.FollowService;
+
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.ConstraintViolationException;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+
+@Classes(cdi = true,
+        value={UnfollowServlet.class},
+        cdiStereotypes = CdiMock.class)
+public class UnfollowServletTest extends ServletTest{
+
+    @Mock private FollowService service;
+    @Mock private HttpServletRequest request;
+    @Mock private HttpServletResponse response;
+    @Inject UnfollowServlet unfollowServlet;
+
+    @Test
+    void successfulldoPost() throws ServletException, IOException{
+        when(request.getParameter("section")).thenReturn("1");
+        unfollowServlet.doPost(request,response);
+        verify(service, times(1)).unFollow(anyInt());
+    }
+
+    @Test
+    void faildoPost() throws ServletException, IOException{
+        when(request.getParameter("section")).thenReturn("1");
+        doThrow(ConstraintViolationException.class).when(service).unFollow(anyInt());
+        assertThrows(ConstraintViolationException.class,() -> unfollowServlet.doPost(request,response));
+    }
+
+
+    @Test
+    void successfulldoGet() throws ServletException, IOException{
+        when(request.getParameter("section")).thenReturn("1");
+        unfollowServlet.doGet(request,response);
+        verify(service, times(1)).unFollow(anyInt());
+    }
+
+    @Test
+    void faildoGet() throws ServletException, IOException{
+        when(request.getParameter("section")).thenReturn("1");
+        doThrow(ConstraintViolationException.class).when(service).unFollow(anyInt());
+        assertThrows(ConstraintViolationException.class,() -> unfollowServlet.doGet(request,response));
+    }
+
+
+}
