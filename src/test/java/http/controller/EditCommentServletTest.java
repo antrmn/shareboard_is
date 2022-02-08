cf3aa3c0b48ab4203cc5b1ad5ad3691c3fd0c102
Zane <alessandro.demaio@virgilio.it>
2022-02-08 01:41:14 +0100

    -HomeServletTest -EditCommentTest -DeleteUserTest

--- src/test/java/http/controller/EditCommentServletTest.java
+++ src/test/java/http/controller/EditCommentServletTest.java
@@ -5,8 +5,7 @@ import org.apache.openejb.testing.Classes;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import rocks.limburg.cdimock.CdiMock;
-import service.AuthenticationService;
-import service.dto.CurrentUser;
+import service.CommentService;
 
 import javax.inject.Inject;
 import javax.servlet.RequestDispatcher;
@@ -22,64 +21,58 @@ import static org.mockito.Mockito.*;
 
 
 @Classes(cdi = true,
-        value={LoginServlet.class},
+        value={EditCommentServlet.class},
         cdiStereotypes = CdiMock.class)
-public class LoginServletTest extends ServletTest{
+public class EditCommentServletTest extends ServletTest{
 
-    @Mock AuthenticationService authenticationService;
-    @Mock CurrentUser currentUser;
+    @Mock private CommentService service;
     @Mock private HttpServletRequest request;
     @Mock private HttpServletResponse response;
     @Mock private RequestDispatcher dispatcher;
-    @Inject LoginServlet loginServlet;
+    @Inject EditCommentServlet editCommentServlet;
 
 
     @Test
     void successfulldoGet() throws ServletException, IOException{
+
+        when(request.getParameter("id")).thenReturn("1");
+        when(request.getParameter("text")).thenReturn("text");
+
         when(request.getRequestDispatcher(anyString())).thenReturn(dispatcher);
-        LoginServlet spyServlet = spy(loginServlet);
+        when(request.getRequestDispatcher(anyString())).thenReturn(dispatcher);
+        EditCommentServlet spyServlet = spy(editCommentServlet);
         ServletContext servletContext = mock(ServletContext.class);
         when(servletContext.getContextPath()).thenReturn("path");
         doReturn(servletContext).when(spyServlet).getServletContext();
-        loginServlet.doGet(request,response);
-        if(currentUser.isLoggedIn())
-            verify(response, times(1)).sendRedirect(any());
-        else
-            verify(dispatcher, times(1)).forward(any(), any());
+        editCommentServlet.doGet(request,response);
+        verify(service, times(1)).editComment(any(), any());
     }
 
     @Test
     void faildoGet() throws ServletException, IOException{
         when(request.getRequestDispatcher(anyString())).thenReturn(dispatcher);
         doThrow(ConstraintViolationException.class).when(response).sendRedirect(any());
-        doThrow(ConstraintViolationException.class).when(dispatcher).forward(any(), any());
-        assertThrows(ConstraintViolationException.class,() -> loginServlet.doGet(request,response));
+        assertThrows(ConstraintViolationException.class,() -> editCommentServlet.doGet(request,response));
     }
 
-    //TODO: CHECK
     @Test
     void successfulldoPost() throws ServletException, IOException{
         when(request.getRequestDispatcher(anyString())).thenReturn(dispatcher);
         when(request.getParameter("username")).thenReturn("username");
         when(request.getParameter("pass")).thenReturn("pass");
-        LoginServlet spyServlet = spy(loginServlet);
+        EditCommentServlet spyServlet = spy(editCommentServlet);
         ServletContext servletContext = mock(ServletContext.class);
         when(servletContext.getContextPath()).thenReturn("path");
-        when(authenticationService.authenticate(any(), any())).thenReturn(false);
         doReturn(servletContext).when(spyServlet).getServletContext();
-        loginServlet.doPost(request,response);
-        if(currentUser.isLoggedIn())
-            verify(response, times(1)).sendRedirect(any());
-        else
-            verify(dispatcher, times(1)).forward(any(), any());
+        editCommentServlet.doPost(request,response);
+        verify(response, times(1)).sendRedirect(any());
     }
 
     @Test
     void faildoPost() throws ServletException, IOException{
         when(request.getRequestDispatcher(anyString())).thenReturn(dispatcher);
         doThrow(ConstraintViolationException.class).when(response).sendRedirect(any());
-        doThrow(ConstraintViolationException.class).when(dispatcher).forward(any(), any());
-        assertThrows(ConstraintViolationException.class,() -> loginServlet.doPost(request,response));
+        assertThrows(ConstraintViolationException.class,() -> editCommentServlet.doPost(request,response));
     }
 
 
---
+++ src/test/java/http/controller/HomeServletTest.java
@@ -0,0 +1,60 @@
+package http.controller;
+
+
+import org.apache.openejb.testing.Classes;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import rocks.limburg.cdimock.CdiMock;
+import javax.inject.Inject;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.ConstraintViolationException;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+
+@Classes(cdi = true,
+        value={HomeServlet.class},
+        cdiStereotypes = CdiMock.class)
+public class HomeServletTest extends ServletTest{
+
+    @Mock private HttpServletRequest request;
+    @Mock private HttpServletResponse response;
+    @Mock private RequestDispatcher dispatcher;
+    @Inject HomeServlet homeServlet;
+
+
+    @Test
+    void successfulldoGet() throws ServletException, IOException{
+        when(request.getRequestDispatcher(anyString())).thenReturn(dispatcher);
+        homeServlet.doGet(request,response);
+        verify(dispatcher, times(1)).forward(any(), any());
+    }
+
+    @Test
+    void faildoGet() throws ServletException, IOException{
+        when(request.getRequestDispatcher(anyString())).thenReturn(dispatcher);
+        doThrow(ConstraintViolationException.class).when(dispatcher).forward(any(), any());
+        assertThrows(ConstraintViolationException.class,() -> homeServlet.doGet(request,response));
+    }
+
+    @Test
+    void successfulldoPost() throws ServletException, IOException{
+        when(request.getRequestDispatcher(anyString())).thenReturn(dispatcher);
+        homeServlet.doPost(request,response);
+        verify(dispatcher, times(1)).forward(any(), any());
+    }
+
+    @Test
+    void faildoPost() throws ServletException, IOException{
+        when(request.getRequestDispatcher(anyString())).thenReturn(dispatcher);
+        doThrow(ConstraintViolationException.class).when(dispatcher).forward(any(), any());
+        assertThrows(ConstraintViolationException.class,() -> homeServlet.doPost(request,response));
+    }
+
+
+}
