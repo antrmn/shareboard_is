7b38b7b79eb1779c28cfb2897fb34828cba07c55
ilgrafico <61851614+ilgrafico@users.noreply.github.com>
2022-02-12 13:18:15 +0100

    Fix PostServletTest.

--- src/test/java/http/controller/PostServletTest.java
+++ src/test/java/http/controller/PostServletTest.java
@@ -4,9 +4,11 @@ package http.controller;
 import org.apache.openejb.testing.Classes;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import persistence.model.Comment;
 import rocks.limburg.cdimock.CdiMock;
 import service.CommentService;
-import service.PostService;;
+import service.PostService;
+import service.dto.CommentDTO;;
 import javax.inject.Inject;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
@@ -15,6 +17,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.ConstraintViolationException;
 import java.io.IOException;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
@@ -40,6 +47,28 @@ public class PostServletTest extends ServletTest{
         when(request.getParameter("comment")).thenReturn("0");
         when(request.getRequestDispatcher(anyString())).thenReturn(dispatcher);
 
+
+        PostServlet spyServlet = spy(postServlet);
+        ServletContext servletContext = mock(ServletContext.class);
+        when(servletContext.getContextPath()).thenReturn("path");
+        doReturn(servletContext).when(spyServlet).getServletContext();
+        postServlet.doPost(request,response);
+        verify(dispatcher, times(1)).forward(any(), any());
+    }
+
+    @Test
+    void successfulldoPostWithCommentId() throws ServletException, IOException{
+
+        when(request.getParameter("id")).thenReturn("0");
+        when(request.getParameter("comment")).thenReturn("1");
+        when(request.getRequestDispatcher(anyString())).thenReturn(dispatcher);
+
+        Map<Integer, List<CommentDTO>> comments = new HashMap<>();
+        List<CommentDTO> list = new ArrayList<>();
+        CommentDTO commentDTO = new CommentDTO(1,"author",2,"content", Instant.now(),3,4,1,3);
+        list.add(commentDTO);
+        comments.put(1,list);
+        doReturn(comments).when(commentService).getReplies(anyInt());
 
         PostServlet spyServlet = spy(postServlet);
         ServletContext servletContext = mock(ServletContext.class);
