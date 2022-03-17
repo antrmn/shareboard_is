07b5081fd0cb5f96641c3a9b0e7c0966a10a9f09
Antonio <ar0x2ff@gmail.com>
2022-03-17 17:58:48 +0100

    aggiunto flyway, immagini sample, aggiornato sql

--- src/main/java/common/FlywayMigrator.java
+++ src/main/java/common/FlywayMigrator.java
@@ -2,34 +2,72 @@ package common;
 
 import org.flywaydb.core.Flyway;
 import org.flywaydb.core.api.configuration.FluentConfiguration;
+import org.flywaydb.core.api.output.MigrateResult;
 
-import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
-import javax.ejb.*;
+import javax.ejb.TransactionManagement;
+import javax.ejb.TransactionManagementType;
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.Initialized;
+import javax.enterprise.event.Observes;
 import javax.sql.DataSource;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
-@Singleton
-@Startup
 @TransactionManagement(TransactionManagementType.BEAN)
 class FlywayMigrator {
+
+    private Path uploadRoot = Path.of(System.getProperty("openejb.home"), "uploads");
 
     @Resource(name = "jdbc/shareboardDB")
     private DataSource dataSource;
 
-    @PostConstruct
-    private void onStartup(){
+    public void postConstruct(@Observes @Initialized(ApplicationScoped.class) Object o) throws IOException {
         if (dataSource == null) {
-            throw new EJBException("Cannot migrate, no datasource configured!");
+            throw new RuntimeException("Cannot migrate, no datasource configured!");
         }
         FluentConfiguration config = Flyway.configure()
                 .dataSource(dataSource)
-                .createSchemas(true)
                 .validateOnMigrate(false)
                 .locations("classpath:db/migration");
         Flyway flyway = new Flyway(config);
-        flyway.migrate();
+        MigrateResult migrate = flyway.migrate();
+        if(!migrate.migrations.isEmpty()){
+            Files.createDirectories(uploadRoot);
+
+            InputStream resourceAsStream = getClass().getClassLoader().getResourceAsStream("sample/pictures.zip");
+            if(resourceAsStream == null){
+                throw new RuntimeException("pictures.zip not found");
+            }
+
+            try(ZipInputStream zipStream = new ZipInputStream(resourceAsStream)){
+                ZipEntry entry = zipStream.getNextEntry();
+                byte[] buffer = new byte[1024];
+                while (entry != null) {
+                    File newFile = new File(uploadRoot.toFile(), entry.getName());
+                    // fix for Windows-created archives
+                    File parent = newFile.getParentFile();
+                    if (!parent.isDirectory() && !parent.mkdirs()) {
+                        throw new IOException("Failed to create directory " + parent);
+                    }
+
+                    // write file content
+                    try(FileOutputStream fos = new FileOutputStream(newFile)){
+                        int len;
+                        while ((len = zipStream.read(buffer)) > 0) {
+                            fos.write(buffer, 0, len);
+                        }
+                        entry = zipStream.getNextEntry();
+                    }
+                }
+            }
+
+        }
     }
-
-
-
 }
